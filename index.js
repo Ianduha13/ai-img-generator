57cef7117f5f512182823eacd8ed7755101bc6a6
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-07 15:57:53 -0300

    Finishing Image Generation Configs

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use("/openai", require("./routes/openAiRoutes"))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
