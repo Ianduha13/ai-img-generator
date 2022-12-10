1df3a5f25203cb09a60a9eef81c94a8a92e07e57
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 16:34:17 -0300

    fixing server

--- public/ai-img-generator/src/App.js
+++ public/ai-img-generator/src/App.js
@@ -21,7 +21,7 @@ const App = () => {
       const response = await fetch(`${apiUrl}/api/openai/generateimage`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         },
         body: JSON.stringify({
           prompt,
