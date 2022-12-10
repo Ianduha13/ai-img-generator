065b913399ce6be500360c9293700bb981fa4454
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 19:55:26 -0300

    Fixing deploy

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
-const express = require("express")
 const path = require("path")
-const dotenv = require("dotenv").config()
-const cors = require("cors")
-const routes = require("./routes/openAiRoutes")
+const express = require("express")
 
 const app = express()
-app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/openai", routes)
+app.use(express.static(path.join(__dirname, "frontend")))
+
+app.use("/openai", require("./routes/openAiRoutes"))
+
+module.exports = app
