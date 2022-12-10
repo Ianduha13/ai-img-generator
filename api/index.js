45f4dc0e36aab83464bf98eba37ad518e7387c06
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 18:55:18 -0300

    Updating cors

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const express = require("express")
 const path = require("path")
 const dotenv = require("dotenv").config()
 const cors = require("cors")
+const routes = require("./routes/openAiRoutes")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/openai", require("./routes/openAiRoutes"))
+app.use("/openai", routes)
