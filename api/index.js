0c61d0db15fbd4012aa7d6195e28d91c2a07d852
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 16:06:12 -0300

    Adding variables to vercel.json

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
-const path = require("path")
 const express = require("express")
+const path = require("path")
 const cors = require("cors")
+const logger = require("morgan")
 
 const app = express()
-app.use(cors())
-
+app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
-app.use(express.static(path.join(__dirname, "../public/ai-img-generator")))
+app.get("/api/test", (req, res) => {
+  res.send("test")
+})
+
+app.use(
+  express.static(path.join(__dirname, "../public/ai-img-generator/build"))
+)
 
 app.get("/openai", (req, res) => res.end("Open AI API"))
 app.use("/openai", require("./routes/openAiRoutes"))
