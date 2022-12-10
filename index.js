3b50528b135b30ce37d8013e5bad877467979109
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-09 21:04:06 -0300

    configure cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path")
 const express = require("express")
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
