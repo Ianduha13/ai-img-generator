67cef5a054c227852a97be896982f306df4ddf92
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 17:23:59 -0300

    fixing index.js static route

--- api/index.js
+++ api/index.js
@@ -4,19 +4,15 @@ const dotenv = require("dotenv").config()
 const cors = require("cors")
 
 const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
     origin: "https://ai-img-generator-front.vercel.app/",
     methods: ["POST"],
   })
 )
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use(
-  express.static(
-    path.join(__dirname, "../public/ai-img-generator/build/index.html")
-  )
-)
+app.use(express.static(path.join(__dirname, "../frontend/build/index.html")))
 
 app.use("/api/openai", require("./routes/openAiRoutes"))
