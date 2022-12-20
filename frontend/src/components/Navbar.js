8589cff822df9f7181a029aabd78d6703c3d9e0b
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-20 15:23:11 -0300

    Fixing portfolio link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,30 @@
 import "./styles/navbar.css"
 import { FaGithub, FaBriefcase } from "react-icons/fa"
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-left'>
-        <header>Img IA Generator</header>
-      </div>
-      <div className='navbar-right'>
-        <a
-          className='navbar-btn'
-          href='https://github.com/Ianduha13?tab=repositories'
-          target={"_blank"}
-          rel='noreferrer'
-        >
-          <FaGithub size='1.25em' color='white' />
-        </a>
-        <a
-          className='navbar-btn'
-          href='https://my-portfolio-eta-bice.vercel.app/'
-          target={"_blank"}
-          rel='noreferrer'
-        >
-          <FaBriefcase size='1.25em' color='white' />
-        </a>
-      </div>
-    </nav>
-  )
+	return (
+		<nav className='navbar'>
+			<div className='navbar-left'>
+				<header>Img IA Generator</header>
+			</div>
+			<div className='navbar-right'>
+				<a
+					className='navbar-btn'
+					href='https://github.com/Ianduha13?tab=repositories'
+					target={"_blank"}
+					rel='noreferrer'
+				>
+					<FaGithub size='1.25em' color='white' />
+				</a>
+				<a
+					className='navbar-btn'
+					href='https://my-portfolio-ianduhamel.vercel.app/'
+					target={"_blank"}
+					rel='noreferrer'
+				>
+					<FaBriefcase size='1.25em' color='white' />
+				</a>
+			</div>
+		</nav>
+	)
 }
 export default Navbar
